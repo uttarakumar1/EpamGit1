d3c7bbbc93a527df34b7c51443d405fd5d0e2c7f
uttarakumar1 <60352813+uttarakumar1@users.noreply.github.com>
2020-03-09 20:46:38 +0530

    Add files via upload

---
+++ MainTestConstructionCost.java
@@ -0,0 +1,13 @@
+package com.sici;
+
+public class MainTestConstructionCost {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		ConstructionCost cc=new ConstructionCost();
+
+	}
+
+}
+
